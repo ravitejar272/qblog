5e32c2e3d08055606fa5bf72f77a117b71bf7045
Ravi Teja Reddy Bolla <Ravi Teja Reddy Bolla>
2016-12-23 13:22:00 -0500

    Added basic buildspec, appspec install and run scripts

---
+++ scripts/install.sh
@@ -0,0 +1,3 @@
+sudo apt-get install python-pip
+sudo apt-get install python-dev build-essential
+sudo pip install --upgrade pip
---
+++ scripts/run.sh
@@ -0,0 +1,3 @@
+pip install django-markdown
+python manage.py migrate
+python manage.py runserver &
